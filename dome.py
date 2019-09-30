45dbe5e4ecbbd9dfc6d949b2eaac033680d39e24
Plop3 <serge@plop3.fr>
2019-09-30 10:44:40 +0200

    Ajout du code python

---
+++ dome.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- encoding: utf8 -*-
+
+
+
+# Pilotage automatique de l'abri du télescope.
+# Serge CLAUS
+# GPL V3
+# Version 1.0.0
+# 30/09/2019
+# Version python3 pour Raspberry PI
+
+##### FICHIERS EXTERNES #####
+
+##### PERIPHERIQUES #####
+
+##### CONSTANTES #####
+
+##### MACROS #####
+
+##### VARIABLES GLOBALES #####
+
+##### SETUP ######
+# Initialistion des entrées/sorties
+
+##### BOUCLE PRINCIPALE #####
+
+##### FONCTIONS #####
+
+
