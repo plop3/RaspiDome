b47df62a18a48b86b3e1fdcb82c1acc77a74d54f
Plop3 <serge@plop3.fr>
2019-10-11 12:22:50 +0200

    Version de test

--- dome.py
+++ dome.py
@@ -2,19 +2,12 @@
 # -*- encoding: utf8 -*-
 
 
-
 # Pilotage automatique de l'abri du télescope.
 # Serge CLAUS
 # GPL V3
 # Version 1.0.0
 # 30/09/2019
 # Version python3 pour Raspberry PI
-
-
-# TODO
-# Remplacer POSDOME par les entrées capteurs
-# Idem pour PORTEOUV
-# AttendARU à faire
 
 ##### MODULES EXTERNES #####
 import sys
@@ -24,114 +17,144 @@ import threading
 # Firmata
 from pymata_aio.pymata3 import PyMata3
 from pymata_aio.constants import Constants
-board = PyMata3()
+board = PyMata3(com_port='/dev/ttyUSB1')
 
-##### PERIPHERIQUES #####
-##### CONSTANTES #####
-##### MACROS #####
-##### VARIABLES GLOBALES #####
 CMD=''
-PORTEOUV=False
-POSDOME=False
-
 
 # Entrées/sorties
-AO=17
-AF=16
-Po1=18
-Po2=19
-Pf1=20
-Pf2=21
+AO=17	#A1
+AF=16	#A0
+Po1=18	#A2
+Po2=19	#A3
+Pf1=20	#A4
+Pf2=21	#A5
 
-MOTEUR=11	
-ALIMMOT=10	
-ALIM12=9	
-ALIMTEL=8	
-P11=7		
-P12=6		
-P21=5		
-P22=4
+PARK = 22	#A6
 
-PARK = 13
+MOTEUR=11	#D11
+ALIMMOT=10	#D10
+ALIM12=9	#D9
+ALIMTEL=8	#D8
+P11=7		#D7
+P12=6		#D6
+P21=5		#D5
+P22=4		#D4
 
 # Délais
 DPORTES=40
 DPORTESCAPTEURS=30
 DMOTEUR=40
-DABRI=20
-
-PINPARK=True
+DABRI=22
 
 ##### FONCTIONS #####
-
+def PStatus(pin):
+	if pin<16:
+		return board.get_pin_state(pin)[2]
+	elif pin > 21:
+		if board.analog_read(pin-16)> 300:
+			return True
+		else:
+			return False
+	else:
+		return board.digital_read(pin)
+def Pwrite(pin, etat):
+	board.digital_write(pin, etat)
+def Pinit(pin,mode):
+	board.set_pin_mode(pin, mode)
+		
 def AlimStatus():
-	return not board.digital_read(ALIMTEL)
-def PortesOuvert():
-	return (not board.digital_read(Po1) and not board.digital_read(Po2))
-def PortesFerme():
-	return (not board.digital_read(Pf1) and not board.digital_read(Pf2))
-def AbriFerme():
-	return not board.digital_read(AF)
-def AbriOuvert():
-	return not board.digital_read(AO)
+	return not PStatus(ALIMTEL)
 def MoteurStatus():
-	return not board.get_pin_state(ALIMMOT)[2] #board.digital_read(ALIMMOT)
+	return not PStatus(ALIMMOT)
 def StartTel():
-	board.digital_write(ALIMTEL,0)
+	Pwrite(ALIMTEL,0)
 def StopTel():
-	board.digital_write(ALIMTEL,1)
+	Pwrite(ALIMTEL,1)
 def StartMot():
-	board.digital_write(ALIMMOT,0)
+	Pwrite(ALIMMOT,0)
 def StopMot(): 
-	board.digital_write(ALIMMOT,1)
+	Pwrite(ALIMMOT,1)
+def PortesOuvert():
+	return not PStatus(Po1) and not PStatus(Po2)
+def PorteFerme():
+	return not PStatus(Pf1)	# Pas de capteur sur la porte 2 pour l'instant
+def AbriOuvert():
+	return not PStatus(AO)
+def AbriFerme():
+	return not PStatus(AF)
 def TelPark():
-	return 1
+	return PStatus(PARK)
 
-def ARU():
-	# Arret d'urgence
-	# TODO A créer
-	pass
+def ARU(msg):
+	Pwrite(ALIMMOT,1)
+	Pwrite(ALIMTEL,1)
+	Pwrite(ALIM12,1)
+	Prite(Po1,1)
+	Prite(Po2,1)
+	Prite(Pf1,1)
+	Prite(Pf2,1)
+	# On prévient du problème
+	Debug('ARU '+msg)
+	# TODO Attente d'une commnande de déblocage
+	while True:
+		time.sleep(0.1)
+	sys.exit(1)
 	
-def AttendARU(delai,park,depl):
-	# TODO Arrêt à distance
-	# TODO compléter
+def delai():
+	pass
+def Attend(delai,park,depl,porte):
+	t=threading.Timer(delai)
+	t.start()
 	nbpark=0
+	nbpos=0
+	nbporte=0
 	errmax=2
-	for i in range(0):
+	while t.is_alive():
+		# Surveillance du déplacement
 		if park:
 			if not TelPark():
-				nbpark=nbpark+1
+				nbpark+=1
+			else: 
+				nbpark=0
 			if nbpark > errmax:
-				ARU()
+				ARU('Erreur Park')
 		if depl:
 			if not AbriFerme() and not AbriOuvert():
-				ARU()
-	time.sleep(delai)
-	
+				nbpos+=1
+			else:
+				nbpos=0
+			if nbpos > errmax:	
+				ARU('Erreur position abri')
+		# TODO Décommenter quand les capteurs portes seront cablés
+		if porte:
+			if not PortesOuvert():
+				nbporte+=1
+			else:
+				nbporte=0
+			if nbporte > errmax:
+				ARU('Erreur portes')
+		time.sleep(0.1)
+		
 def FermePorte1():
-	board.digital_write(P11,0)
+	Pwrite(P11,0)
 	time.sleep(DPORTES)
-	board.digital_write(P11,1)
+	Pwrite(P11,1)
 	
 def OuvrePorte1():
-	board.digital_write(P12,0)
+	Pwrite(P12,0)
 	time.sleep(DPORTES)
-	board.digital_write(P12,1)
+	Pwrite(P12,1)
 	
 def CmdTelnet():
-	global CMD
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind(('',1234))
-	s.listen(1)
 	global conn
-	print('START')
-	while True:
+	try:
 		conn, addr = s.accept()
-		# Lecture des données sur le port
-		if CMD=='':
-			CMD = conn.recv(32)
-		time.sleep(1)
-		
+		CMD = conn.recv(32)
+	except BlockingIOError:
+		return ''
+	else:
+		return CMD
+				
 def EnvoiStatus(ret):
 	conn.sendall(str(int(ret)).encode('utf8'))
 
@@ -147,94 +170,104 @@ def EnvoiCommande(cmd):
 	if CMD==b'D+':
 		EnvoiStatus(OuvreDome())
 	elif CMD==b"D-":
-		FermeDome()
+		EnvoiStatus(FermeDome())
+	elif CMD==b'p-':
+		FermePorte1()
+	elif CMD==b'p+':
+		OuvrePorte1()
 	elif CMD==b'P+':
 		EnvoiStatus(OuvrePortes())
 	elif CMD==b'P-':
 		EnvoiStatus(FermePortes())
 	elif CMD==b'A+':
 		StartTel()
-		EnvoiStatus('1')
+		EnvoiStatus(AlimStatus())
 	elif CMD==b'A-':
 		StopTel()
-		EnvoiStatus('0')
+		EnvoiStatus(AlimStatus())
 	elif CMD==b'A?':
 		EnvoiStatus(AlimStatus())
 	elif CMD==b'P?':
 		EnvoiStatus(PortesOuvert())
 	elif CMD==b'D?':
-		EnvoiStatus(AbriFerme())
-	elif CMD==b'AU':
-		EnvoiStatus('0')
-		ARU()
-	elif CMD==b'p-':
-		FermePorte1()
-	elif CMD==b'p+':
-		OuvrePorte1()
-	elif CMD==b'C?':
-		EnvoiStatus(AbriFerme())
 		EnvoiStatus(AbriOuvert())
-		EnvoiStatus(PortesFerme())
+	elif CMD==b'C?':
+		EnvoiStatus(AbriOuvert())
+		EnvoiStatus(AbriFerme())
 		EnvoiStatus(PortesOuvert())
+		EnvoiStatus(PortesFerme())
 		EnvoiStatus(AlimStatus())
-		if PINPARK:
-			EnvoiMsg('p')
+		if TelPark():
+			EnvoiStatus('p')
 		else:
-			EnvoiMsg('n')
-		# Park
-	CMD=''
+			EnvoiStatus('n')
 	conn.close()
 
-def Affiche(message):
+def Debug(message):
 	# Affiche les informations
 	print(message)
 	
 def OuvrePortes():
-	global PORTEOUV
-	if PORTEOUV:
+	if PortesOuvert():
 		# Les portes sont déjà ouvertes
 		return 0
+	if not AbriFerme() and not AbriOuvert():
+		# Abri non fermé, fermeture des portes impossible
+		Debug("Fermeture des portes impossible: Position incorrecte de l'abri.")
+		return 0
 	StartMot()
-	Affiche('Ouverture des portes...')
-	board.digital_write(P12,0)
-	Affiche('Ouverture porte 1...')
-	AttendARU(5,False,False)
-	Affiche('Ouverture porte 2...')
-	board.digital_write(P22,0)
-	AttendARU(DPORTESCAPTEURS,False,False)
-	#while not PortesOuvert():
-	#	AttendARU(0.1,False,False)
-	AttendARU(5,False,False)
-	board.digital_write(P12,1)
-	board.digital_write(P22,1)
-	Affiche ('Portes ouvertes')
-	PORTEOUV=True
+	Debug('Ouverture des portes...')
+	Pwrite(P12,0)
+	Debug('Ouverture porte 1...')
+	Attend(5,1,1,0)
+	Debug('Ouverture porte 2...')
+	Pwrite(P22,0)
+	Attend(DPORTES,1,1,0)
+	while not PortesOuvert():
+		time.sleep(0.1)
+	Pwrite(P12,1)
+	Pwrite(P22,1)
+	Debug('Portes ouvertes')
 	return 1
 	
 def FermePortes():
-	global PORTEOUV
-	if not PORTEOUV:
+	if not PortesOuvert():
+		Debug('Portes déjà fermées')
 		# Les portes sont déjà fermées
 		return 0
+	if not AbriFerme():
+		# Abri non fermé, fermeture des portes impossible
+		Debug('Fermeture des portes impossible: Abri non fermé. ')
+		return 0
+	# TODO Voir si utile de vérifier l'état Park
 	StopMot()
-	board.digital_write(P21,0)
-	Affiche('Fermeture des portes...')
-	Affiche('Fermeture porte 2...')
-	AttendARU(5,False,False)
-	board.digital_write(P11,0)
-	Affiche('Fermeture porte 1...')
-	AttendARU(DPORTES,False,False)
-	Affiche('Portes fermées')
-	board.digital_write(P11,1)
-	board.digital_write(P21,1)
-	PORTEOUV=False
+	Pwrite(P21,0)
+	Debug('Fermeture des portes...')
+	Debug('Fermeture porte 2...')
+	Attend(5,1,1,0)
+	Pwrite(P11,0)
+	Debug('Fermeture porte 1...')
+	Attend(DPORTES,1,1,0)
+	Debug('Portes fermées')
+	Pwrite(P11,1)
+	Pwrite(P21,1)
+	if PortesOuvert():
+		# Problème de fermeture des portes
+		Debug("Problème de fermeture des portes")
+		return 0
 	return 1
 	
 def DeplaceDome(sens):
-	global POSDOME
-	if not PINPARK:
-		Affiche("Erreur: télescope non parqué")
-		return False
+	EtatAbri=AbriFerme()	# Enregistre la position actuelle de l'abri
+	if (not AbriFerme() and not AbriOuvert()) or (AbriFerme() and AbriOuvert()):
+		# Problème de capteur
+		Debug('Problème de capteur de position abri')
+		return 0
+	if not TelPark():
+		# Télescope non parqué
+		# TODO Tenter de parquer le télescope
+		Debug('Télescope non parqué')
+		return 0
 	StopTel()
 	if not PortesOuvert:
 		if not MoteurStatus():
@@ -242,102 +275,111 @@ def DeplaceDome(sens):
 		OuvrePortes()
 	elif not MoteurStatus():
 		StartMot()
-		AttendARU(DMOTEUR,True, False)
-	print('Demarrage moteur')
-	board.digital_write(MOTEUR,0)
+		Attend(DMOTEUR,True, False,False)
+	OuvrePortes()
+	Debug('Demarrage moteur')
+	Pwrite(MOTEUR,0)
 	time.sleep(0.6)
-	board.digital_write(MOTEUR,1)
-	AttendARU(DABRI,True, False)
-	# TODO Supprimer les commentaires ci-dessous
-	##while (not AbriOuvert() and not AbriFerme())
-	##	AttendARU(1,True, False)
-	AttendARU(2, True , False)
-	# TODO Commuter les lignes suivantes (test sans capteurs)
-	POSDOME=not POSDOME
-	# POSDOME = not AbriOuvert()
-	if POSDOME:
-		# Abri ouvert
-		StartTel()
-		time.sleep(0.5)
+	Pwrite(MOTEUR,1)
+	Attend(DABRI,1,0,1)
+	while (not AbriOuvert() and not AbriFerme()):
+		Attend(1,True, False, True)
+	if EtatAbri==AbriOuvert():
+		Debug("L'abri ne s'est pas déplacé")
+		return 0
+	elif AbriOuvert() or AbriFerme():
+		return 1
 	else:
-		# Abri fermé
-		FermePortes()
-		StopMot()
-		time.sleep(0.5)
-	
+		Debug("Position incorrecte de l'abri")
+		# Abri ni ouvert, ni fermé :-(
+		ARU("Erreur de position abri")
+		return 0
+			
 def OuvreDome():
-	global POSDOME
-	Affiche('Ouverture dome...')
-	if POSDOME:
-		Affiche('Erreur: Abri déjà ouvert')
-		return False
-	DeplaceDome(True)
-	Affiche('Dome ouvert')
-	POSDOME=True
-	return 1
+	Debug('Ouverture dome...')
+	if AbriOuvert():
+		Debug('Erreur: Abri déjà ouvert')
+		return 0
+	StartTel()
+	if DeplaceDome(True):
+		Debug('Dome ouvert')
+		return 1
+	else:
+		Debug("Problème déplacement dome")
+		StopTel()
+		return 0
 
 def FermeDome():
-	global POSDOME
-	Affiche('Fermeture dome...')
-	if not POSDOME:
-		Affiche('Erreur: Abri déjà fermé')
-		return False
-	DeplaceDome(False)
-	FermePortes()
-	Affiche('Dome fermé')
-	POSDOME=False
-	return 1
+	Debug('Fermeture dome...')
+	if AbriFerme():
+		Debug('Erreur: Abri déjà fermé')
+		return 0
+	if DeplaceDome(False):
+		StopMot()
+		FermePortes()
+		StopTel()
+		Debug('Dome fermé')
+		return 1
+	else:
+		Debug("Problème déplacement dome")
+		return 0
 	
 ##### SETUP ######
 # Initialisation des entrées/sorties
 # Initialisation du port série, du socket
 
-# Entrées
-board.set_pin_mode(AO,Constants.PULLUP)
-board.set_pin_mode(AF,Constants.PULLUP)
-board.set_pin_mode(Po1,Constants.PULLUP)
-board.set_pin_mode(Po2,Constants.PULLUP)
-board.set_pin_mode(Pf1,Constants.PULLUP)
-board.set_pin_mode(Pf2,Constants.PULLUP)
-
 # Sorties
-board.set_pin_mode(ALIM12,Constants.OUTPUT)
-board.digital_write(ALIM12,1)
-board.set_pin_mode(ALIMTEL,Constants.OUTPUT)
-board.digital_write(ALIMTEL,1)
-board.set_pin_mode(ALIMMOT,Constants.OUTPUT)
-board.digital_write(ALIMMOT,1)
-board.set_pin_mode(MOTEUR,Constants.OUTPUT)
-board.digital_write(MOTEUR,1)
-board.set_pin_mode(P11,Constants.OUTPUT)
-board.digital_write(P11,1)
-board.set_pin_mode(P12,Constants.OUTPUT)
-board.digital_write(P12,1)
-board.set_pin_mode(P21,Constants.OUTPUT)
-board.digital_write(P21,1)
-board.set_pin_mode(P22,Constants.OUTPUT)
-board.digital_write(P22,1)
 
-print('Fin setup.')
+Pwrite(ALIM12,1)
+Pinit(ALIM12,Constants.OUTPUT)
+Pwrite(ALIMTEL,1)
+Pinit(ALIMTEL,Constants.OUTPUT)
+Pwrite(ALIMMOT,1)
+Pinit(ALIMMOT,Constants.OUTPUT)
+Pwrite(MOTEUR,1)
+Pinit(MOTEUR,Constants.OUTPUT)
+Pwrite(P11,1)
+Pinit(P11,Constants.OUTPUT)
+Pwrite(P12,1)
+Pinit(P12,Constants.OUTPUT)
+Pwrite(P21,1)
+Pinit(P21,Constants.OUTPUT)
+Pwrite(P22,1)
+Pinit(P22,Constants.OUTPUT)
+
+# Entrées 
+Pinit(A0,Constants.PULLUP)
+Pinit(AF,Constants.PULLUP)
+Pinit(Po1,Constants.PULLUP)
+Pinit(Po2,Constants.PULLUP)
+Pinit(Pf1,Constants.PULLUP)
+Pinit(Pf2,Constants.PULLUP)
+Pinit(PARK-16,Constants.ANALOG)
+
+# Socket de communication réseau
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('',1234))
+s.listen(1)
+s.setblocking(0)
+
+Debug('Fin setup.')
 
 # Etat du dome initialisation des interrupteurs
 if AbriOuvert():
 	StartTel()
 	StartMot()
 ##### BOUCLE PRINCIPALE #####
-tkb = threading.Thread(target=CmdTelnet)
-tkb.start()
-time.sleep(2)
-
 
 while True:
 	try:
+		CMD = CmdTelnet()
 		if CMD !='':
 			EnvoiCommande(CMD)
 			# MAJEtatPark() # Interruption ?
 			# Dome bouge ?
 			# Bouton arret d'urgence'
-			time.sleep(1)
 	except KeyboardInterrupt:
 		raise
+	time.sleep(0.5)
 
